311c88f1dc7dc92653b58b1bf4d88ebf3c0c176f
Adam Rainsby <adamrainsby@live.co.uk>
2017-10-30 02:19:49 +0000

    Fix subtraction and exponentiation implementation
    And first cut (WIP) at defining operators for working with dual numbers

--- src/dual.cr
+++ src/dual.cr
@@ -1,5 +1,15 @@
 require "./dual/*"
 
+struct Int32
+  def +(number : Dual::Number)
+    Dual::Number.new(self.to_f, 0.0) + number
+  end
+
+  def *(number : Dual::Number)
+    Dual::Number.new(self.to_f, 0.0) * number
+  end
+end
+
 module Dual
   class Number
     property real : Float64
@@ -15,7 +25,7 @@ module Dual
     end
 
     def -(x : Dual::Number)
-      Dual::Number.new(real * x.real, real * x.dual + dual * x.real)
+      Dual::Number.new(real - x.real, dual - x.dual)
     end
 
     def +(x : Dual::Number)
@@ -23,10 +33,7 @@ module Dual
     end
 
     def **(power : Float64)
-      Dual::Number.new(real ** power, real * power * (real ** (power - 1)))
+      Dual::Number.new(real ** power, power * (real ** (power - 1)))
     end
   end
 end
-
-result = Dual::Number.new(3.0, 1.0) * Dual::Number.new(3.0, 1.0)
-p result
